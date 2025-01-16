8fd72688145369f266a733bb5197b50287922f70
RoastedBrotato <waleedajaz1996@gmail.com>
2025-01-16 14:23:16 +0300

    stage-14 test 2

--- src/main.rs
+++ src/main.rs
@@ -134,11 +134,7 @@ fn main() {
                                 }
                             }
                             let number_value: f64 = number_literal.parse().unwrap();
-                            if number_literal.contains('.') {
-                                println!("NUMBER {} {}", number_literal, number_value);
-                            } else {
-                                println!("NUMBER {} {}.0", number_literal, number_value);
-                            }
+                            println!("NUMBER {} {}.0", number_literal, number_value);
                         }
                         'a'..='z' | 'A'..='Z' | '_' => {
                             let mut identifier = char.to_string();
