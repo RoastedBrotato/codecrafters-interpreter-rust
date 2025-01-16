5c358c40eebbe711d8e0bc966ec04cd228affc6b
RoastedBrotato <waleedajaz1996@gmail.com>
2025-01-16 13:40:09 +0300

    stage-6 test

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,34 @@ fn main() {
             let mut has_error = false;
 
             if !file_contents.is_empty() {
-                let file_contents_chars = file_contents.chars();
-                let _ = file_contents_chars.for_each(|char| match char {
-                    '(' => println!("LEFT_PAREN ( null"),
-                    ')' => println!("RIGHT_PAREN ) null"),
-                    '{' => println!("LEFT_BRACE {{ null"),
-                    '}' => println!("RIGHT_BRACE }} null"),
-                    ',' => println!("COMMA , null"),
-                    '.' => println!("DOT . null"),
-                    '+' => println!("PLUS + null"),
-                    '-' => println!("MINUS - null"),
-                    ';' => println!("SEMICOLON ; null"),
-                    '*' => println!("STAR * null"),
-                    ' ' | '\r' | '\t' => {}, // Ignore whitespace
-                    _ => {
-                        writeln!(io::stderr(), "[line 1] Error: Unexpected character: {}", char).unwrap();
-                        has_error = true;
+                let mut file_contents_chars = file_contents.chars().peekable();
+                while let Some(char) = file_contents_chars.next() {
+                    match char {
+                        '(' => println!("LEFT_PAREN ( null"),
+                        ')' => println!("RIGHT_PAREN ) null"),
+                        '{' => println!("LEFT_BRACE {{ null"),
+                        '}' => println!("RIGHT_BRACE }} null"),
+                        ',' => println!("COMMA , null"),
+                        '.' => println!("DOT . null"),
+                        '+' => println!("PLUS + null"),
+                        '-' => println!("MINUS - null"),
+                        ';' => println!("SEMICOLON ; null"),
+                        '*' => println!("STAR * null"),
+                        '=' => {
+                            if file_contents_chars.peek() == Some(&'=') {
+                                file_contents_chars.next();
+                                println!("EQUAL_EQUAL == null");
+                            } else {
+                                println!("EQUAL = null");
+                            }
+                        }
+                        ' ' | '\r' | '\t' => {}, // Ignore whitespace
+                        _ => {
+                            writeln!(io::stderr(), "[line 1] Error: Unexpected character: {}", char).unwrap();
+                            has_error = true;
+                        }
                     }
-                });
+                }
                 println!("EOF  null"); 
             } else {
                 println!("EOF  null");
