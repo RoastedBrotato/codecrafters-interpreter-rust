17edb4d5152a1763862d7d6926eb38fe0076e74e
RoastedBrotato <waleedajaz1996@gmail.com>
2025-01-16 14:07:13 +0300

    stage-13 test 2

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,7 @@ fn main() {
                                 }
                             }
                             let number_value: f64 = number_literal.parse().unwrap();
-                            println!("NUMBER {} {:.1}", number_literal, number_value);
+                            println!("NUMBER {} {}", number_literal, format!("{:.1}", number_value));
                         }
                         _ => {
                             writeln!(io::stderr(), "[line {}] Error: Unexpected character: {}", line_number, char).unwrap();
